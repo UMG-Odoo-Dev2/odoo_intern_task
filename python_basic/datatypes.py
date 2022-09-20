8963d79e0c24a3a180ff83863467847f369cd7c3
MayPhyo159 <maythuit15@gmail.com>
2022-09-20 15:34:35 +0630

    Second

---
+++ python_basic/datatypes.py
@@ -0,0 +1,33 @@
+#Length of string
+x="Hello World"
+print(len(x)) 
+
+
+#First Character of string
+txt = "Hello World"
+a = txt[0]
+print(a) 
+
+#Get the characters from index 2 to index 4(tho)
+txt1="Python is Fantastic"
+print(txt1[2:5])
+
+
+#Convert the value of t1 to upper case.
+t1="Programming"
+print(t1.upper())
+print(t1.lower())
+
+#Return the string without any whitespace at the beginning or the end
+t2="  Python  " 
+print(t2.strip())
+
+#Replace the character P with a J
+t3 = "Python"
+print(t3.replace("P","M"))
+
+#Insert the correct syntax to add a placeholder for the age parameter
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
---
+++ python_basic/functionex1.py
@@ -0,0 +1,22 @@
+#to assign a "init" function to a class
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is "+self.name)
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+p1.myfunc()
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+my_function("Emil", "Tobias", "Linus")
+
+def my_function1(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function1(fname = "Tobias", lname = "Refsnes")
